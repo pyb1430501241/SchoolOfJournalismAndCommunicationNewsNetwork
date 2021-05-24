209d1ff6a3c7948c737e1eb5ead4508bd20cac0b
半梦Oo <1430501241@qq.com>
2021-05-24 08:26:12 +0800

    添加事务, 缓存

--- gateway/src/main/java/com/pdsu/sojacnn/shiro/LoginRealm.java
+++ gateway/src/main/java/com/pdsu/sojacnn/shiro/LoginRealm.java
@@ -80,6 +80,7 @@ public class LoginRealm extends AuthorizingRealm {
      */
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+
         return null;
     }
 
